19abf86df8759f928a21e927be811efd4095fbb7
aliaksandrp7 <84544733+aliaksandrp7@users.noreply.github.com>
2021-11-12 12:49:37 +0300

    Update Program.java

--- src/main/java/ecd8177/Program.java
+++ src/main/java/ecd8177/Program.java
@@ -9,6 +9,7 @@ public class Program{
 
     public static void main(String[] args) {
          System.out.println("2 change from github");
+         System.out.println("3 change from github");
 
         Map<Integer, String> states = new HashMap<Integer, String>();
         states.put(1, "Germany");
